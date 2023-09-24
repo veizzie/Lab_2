4f8c0f14488a3212774f0f83b022db28d56630e8
veizzie <dmitriyshapovalenko2005@gmail.com>
2023-09-25 00:29:26 +0300

    added code

---
+++ lab_02/lab_02/Program.cs
@@ -0,0 +1,130 @@
+﻿class Ar
+{
+    private int n;
+    private int[] a;
+    private int k;
+
+    public int K
+    {
+        get { return k; }
+    }
+    public int N
+    {
+        get { return n; }
+    }
+
+    public Ar(int a, int b, int x)
+    {
+        if (b < a)
+        {
+            throw new Exception ("b ПОВИННО БУТИ БІЛЬШЕ АБО РІВНЕ a.");
+        }
+
+        n = b - a + 1;
+        this.a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            this.a[i] = (a + i) * x;
+            if (this.a[i] > 50)
+                k++;
+        }
+    }
+
+    public Ar(string fileName)
+    {
+        string[] text = File.ReadAllText(fileName).Split(';');
+        n = text.Length;
+        a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = Convert.ToInt32(text[i]);
+            if (a[i] > 50)
+                k++;
+        }
+    }
+
+    public void Print()
+    {
+        Console.Write("Масив: ");
+        foreach (int element in a)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public int P()
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (a[i] % 2 == 0)
+                return i;
+        }
+        return -1;
+    }
+
+
+    public int Sum(int i1, int i2) 
+    {
+        int sum = 0;
+        for (int i = i1; i <= i2; i++)
+        {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Виберіть конструктор: 1 - за введенням a,b,x; 2 - за допомогою назві файлу");
+        int choise = Convert.ToInt32(Console.ReadLine());
+
+        Ar ar;
+        if (choise == 1)
+        {
+            Console.Write("Введіть a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введіть b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введіть x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            ar = new Ar(a, b, x);
+        }
+        else if (choise == 2)
+        {
+            Console.WriteLine("Введіть ім'я файлу:");
+            string fileName = Console.ReadLine();
+
+            ar = new Ar(fileName);
+        }
+        else
+        {
+            Console.WriteLine("Невірно вибрано конструктор.");
+            return;
+        }
+
+        ar.Print();
+
+        Console.WriteLine($"Кількість елементів більше 50: {ar.K}");
+
+        int lastIndex = ar.P();
+        if (lastIndex != -1)
+        {
+            Console.WriteLine($"Індекс останнього парного елемента: {lastIndex}");
+            int sum = ar.Sum(0, lastIndex);
+            Console.WriteLine($"Сума елементів ліворуч від останнього парного елемента: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Парних елементів в масиві немає.");
+        }
+
+    }
+}
---
+++ lab_02_1/lab_02_1/Program.cs
@@ -0,0 +1,114 @@
+﻿class Parallelepiped
+{
+    private double a;
+    private double b;
+    private double c;
+
+    public double A
+    {
+        get { return a; }
+        set
+        {
+            if (value > 0)
+                a = value;
+            else
+                Console.WriteLine("Сторона повинна бути додатньою.");
+        }
+    }
+
+    public double B
+    {
+        get { return b; }
+        set
+        {
+            if (value > 0)
+                b = value;
+            else
+                Console.WriteLine("Сторона повинна бути додатньою.");
+        }
+    }
+
+    public double C
+    {
+        get { return c; }
+        set
+        {
+            if (value > 0)
+                c = value;
+            else
+                Console.WriteLine("Сторона c повинна бути додатньою.");
+        }
+    }
+
+    public Parallelepiped()
+    {
+        a = 0; b = 0; c = 0;
+    }
+
+    public bool Correct()
+    {
+        return (a > 0 && b > 0 && c > 0);
+    }
+
+    public double Area()
+    {
+        if (Correct())
+            return 2 * (a * b + b * c + a * c);
+        else
+            return -1;
+    }
+
+    public double Volume ()
+    {
+        if (Correct())
+            return a * b * c;
+        else 
+            return -1;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Сторона a: {a}");
+        Console.WriteLine($"Сторона b: {b}");
+        Console.WriteLine($"Сторона c: {c}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        try
+        {
+            Parallelepiped parallelepiped = new Parallelepiped();
+
+            Console.Write("Введіть сторону a: ");
+            parallelepiped.A = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть сторону b: ");
+            parallelepiped.B = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть сторону c: ");
+            parallelepiped.C = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nЗначення полів класу: ");
+            parallelepiped.Print();
+
+            if (parallelepiped.Correct())
+            {
+                Console.WriteLine($"\nПлоща поверхні: {parallelepiped.Area()}");
+                Console.WriteLine($"Об'єм: {parallelepiped.Volume()}");
+            }
+            else
+            {
+                Console.WriteLine("\nПаралелепіпед не існує так як одна зі сторін не додатня.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+    }
+}
